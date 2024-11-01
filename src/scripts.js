36ee4c2aa5c0cb3cc19a9b08ce8885583335eb28
GosegoMatlokwe <166373638+GosegoMatlokwe@users.noreply.github.com>
2024-11-01 15:27:11 +0200

    API intergrationg search functionality using the API

--- src/scripts.js
+++ src/scripts.js
@@ -1,10 +1,24 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "atfbe5f85e4440e083c34b77o4ad7e8d";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#form-input");
-  console.log(searchInput.value);
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
-let seachFormElement = document.querySelector("form");
-seachFormElement.addEventListener("submit", handleSearchSubmit);
+let searchFormElement = document.querySelector("form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+searchCity("Johannesburg");
